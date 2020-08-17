c0c7bf1c822008df91b127ea6c5cbee6643c5c45
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-08-17 18:34:59 -0500

    Replace previewedColorSchemes with previewColorSchemes

---
+++ Sources/Luminescence/Extensions/View+previewColorSchemes.swift
@@ -0,0 +1,27 @@
+/**
+* Luminescence
+*/
+
+import SwiftUI
+
+public extension View {
+	func previewColorSchemes(_ colorSchemes: ColorScheme.AllCases) -> some View {
+		ForEach(colorSchemes, id: \.self) { colorScheme in
+			self
+				.preferredColorScheme(colorScheme)
+		}
+	}
+	
+	func previewColorSchemes(_ colorScheme: ColorScheme) -> some View {
+		self
+			.previewColorSchemes([colorScheme])
+	}
+}
+
+@available(*, deprecated, renamed: "previewColorSchemes")
+public extension View {
+	func colorSchemePreviews() -> some View {
+		self
+			.previewColorSchemes(ColorScheme.allCases)
+	}
+}
