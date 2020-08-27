414c5fce8de01bfb08a6d0d52cd8e1d580e0be3b
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-08-27 17:54:13 -0500

    Update UIImpactFeedbackGenerator+extension.swift

--- Sources/Luminescence/Extensions/UIImpactFeedbackGenerator+extension.swift
+++ Sources/Luminescence/Extensions/UIImpactFeedbackGenerator+extension.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-extension UIImpactFeedbackGenerator {
+public extension UIImpactFeedbackGenerator {
 	static func impactOccurred(with style: FeedbackStyle = .medium) {
 		let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: style)
 		impactFeedbackGenerator.impactOccurred()
