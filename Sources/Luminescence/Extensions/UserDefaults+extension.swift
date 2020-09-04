3238f5fd718268df451e0083fd21efbf1e114fdd
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-09-04 12:52:40 -0500

    Create UserDefaults+extension.swift

---
+++ Sources/Luminescence/Extensions/UserDefaults+extension.swift
@@ -0,0 +1,19 @@
+/**
+* Luminescence
+*/
+
+/*
+extension UserDefaults {
+	enum Key: String {
+		case key
+	}
+	
+	func set(_ value: Any?, for key: Key) {
+		set(value, forKey: key.rawValue)
+	}
+	
+	func data(for key: Key) -> Data? {
+		data(forKey: key.rawValue)
+	}
+}
+*/
