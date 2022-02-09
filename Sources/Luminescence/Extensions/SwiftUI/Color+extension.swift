f6a19183812c42173cc977461c3c85241e8005db
Lucas Brown <lucasbrown@lucasbrownish.com>
2022-02-09 04:37:30 -0600

    Replace deprecated `UIColor` initializer with new `UIColor` initializer

--- Sources/Luminescence/Extensions/SwiftUI/Color+extension.swift
+++ Sources/Luminescence/Extensions/SwiftUI/Color+extension.swift
@@ -10,47 +10,48 @@ public extension Color {
 	// MARK: - Standard Colors
 	
 	// MARK: Adaptable Gray Colors
-	static let gray2 = Color(.systemGray2)
-	static let gray3 = Color(.systemGray3)
-	static let gray4 = Color(.systemGray4)
-	static let gray5 = Color(.systemGray5)
-	static let gray6 = Color(.systemGray6)
+    /* static let gray = Color(.systemGray) */
+    static let gray2 = Color(uiColor: .systemGray2)
+	static let gray3 = Color(uiColor: .systemGray3)
+	static let gray4 = Color(uiColor: .systemGray4)
+	static let gray5 = Color(uiColor: .systemGray5)
+	static let gray6 = Color(uiColor: .systemGray6)
 	
 	// MARK: - UI Element Colors
 	
 	// MARK: Label Colors
-	static let label = Color(.label)
-	static let secondaryLabel = Color(.secondaryLabel)
-	static let tertiaryLabel = Color(.tertiaryLabel)
-	static let quaternaryLabel = Color(.quaternaryLabel)
+	static let label = Color(uiColor: .label)
+	static let secondaryLabel = Color(uiColor: .secondaryLabel)
+	static let tertiaryLabel = Color(uiColor: .tertiaryLabel)
+	static let quaternaryLabel = Color(uiColor: .quaternaryLabel)
 	
 	// MARK: Fill Colors
-	static let systemFill = Color(.systemFill)
-	static let secondarySystemFill = Color(.secondarySystemFill)
-	static let tertiarySystemFill = Color(.tertiarySystemFill)
-	static let quaternarySystemFill = Color(.quaternarySystemFill)
+	static let systemFill = Color(uiColor: .systemFill)
+	static let secondarySystemFill = Color(uiColor: .secondarySystemFill)
+	static let tertiarySystemFill = Color(uiColor: .tertiarySystemFill)
+	static let quaternarySystemFill = Color(uiColor: .quaternarySystemFill)
 	
 	// MARK: Text Colors
-	static let placeholderText = Color(.placeholderText)
+	static let placeholderText = Color(uiColor: .placeholderText)
 	
 	// MARK: Standard Content Background Colors
 	// Use these colors for standard table views and designs that have a white primary background in a light environment.
-	static let systemBackground = Color(.systemBackground)
-	static let secondarySystemBackground = Color(.secondarySystemBackground)
-	static let tertiarySystemBackground = Color(.tertiarySystemBackground)
+	static let systemBackground = Color(uiColor: .systemBackground)
+	static let secondarySystemBackground = Color(uiColor: .secondarySystemBackground)
+	static let tertiarySystemBackground = Color(uiColor: .tertiarySystemBackground)
 	
 	// MARK: Grouped Content Background Colors
 	// Use these colors for grouped content, including table views and platter-based designs.
-	static let systemGroupedBackground = Color(.systemGroupedBackground)
-	static let secondarySystemGroupedBackground = Color(.secondarySystemGroupedBackground)
-	static let tertiarySystemGroupedBackground = Color(.tertiarySystemGroupedBackground)
+	static let systemGroupedBackground = Color(uiColor: .systemGroupedBackground)
+	static let secondarySystemGroupedBackground = Color(uiColor: .secondarySystemGroupedBackground)
+	static let tertiarySystemGroupedBackground = Color(uiColor: .tertiarySystemGroupedBackground)
 	
 	// MARK: Separator Colors
-	static let separator = Color(.separator)
-	static let opaqueSeparator = Color(.opaqueSeparator)
+	static let separator = Color(uiColor: .separator)
+	static let opaqueSeparator = Color(uiColor: .opaqueSeparator)
 	
 	// MARK: Link Color
-	static let link = Color(.link)
+	static let link = Color(uiColor: .link)
 }
 
 #endif
